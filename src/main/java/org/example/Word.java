1ba2a1f5d045119d71ffac8e67f02d22bd9f4dd1
junhyungparker <parker.jhg@gmail.com>
2022-09-02 22:25:27 +0900

    update and create classes

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -1,20 +1,40 @@
 package org.example;
 
 public class Word {
-    private String name;
+    private int id;
+    private int level;
+    private String word;
     private String meaning;
 
-    public Word(String name, String meaning) {
-        this.name = name;
+    public Word(int id, int level, String name, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
         this.meaning = meaning;
     }
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
     }
 
     public String getMeaning() {
