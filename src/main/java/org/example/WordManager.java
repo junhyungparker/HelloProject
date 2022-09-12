e1b5bf98668a0f84d90455be0b7da8c3a5340159
junhyungparker <parker.jhg@gmail.com>
2022-09-12 21:03:04 +0900

    added functions

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -28,15 +28,30 @@ public class WordManager {
     }
 
     public void start(){
+        wordCRUD.loadFile();
         while(true) {
             int menu = selectMenu();
             if(menu == 0) break;
             if(menu == 4 ){
-                wordCRUD.addWord();
+                wordCRUD.addItem();
             }
             else if(menu == 1){
                 wordCRUD.listAll();
             }
+            else if(menu == 2){
+                wordCRUD.searchLevel();
+            }
+            else if(menu == 5){
+                wordCRUD.updateItem();
+            }
+            else if(menu == 6){
+                wordCRUD.deleteItem();
+            }
+            else if(menu == 7){
+                wordCRUD.saveFile();
+            }
         }
     }
+
+
 }
